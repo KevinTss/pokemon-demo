34eda5627578036afce66fd449e53712aea6ef1a
Kevin <kevin.tassi@gmail.com>
2022-10-09 15:16:57 +0200

    Install bootstrap for react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const getPokemonByName = (list, name) => {
   if (!name || !list?.length) return null;
