47ed65668ba1487e08d31fd06dab6b70d34bb673
Kevin <kevin.tassi@gmail.com>
2022-10-09 14:48:34 +0200

    Displaying data the page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [pokemons, setPokemons] = useState([]);
+
   useEffect(() => {
     fetch('https://pokeapi.co/api/v2/pokemon')
       .then((e) => e.json())
       .then((response) => {
-        console.log('res', response);
+        const pokemonList = response.results;
+        setPokemons(pokemonList);
       })
       .catch(console.warn);
   }, []);
 
-  return <div className='App'>pokemon</div>;
+  return (
+    <div className='App'>
+      <ul>
+        {pokemons.map((pokemon) => (
+          <li key={pokemon.name}>{pokemon.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
