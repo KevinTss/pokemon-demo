4b416b61fe8fa9ad5c50fd13d73fbd59333e1484
Kevin <kevin.tassi@gmail.com>
2022-10-09 14:43:23 +0200

    Fetch pokemon from server

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon')
+      .then((e) => e.json())
+      .then((response) => {
+        console.log('res', response);
+      })
+      .catch(console.warn);
+  }, []);
+
   return <div className='App'>pokemon</div>;
 }
 
