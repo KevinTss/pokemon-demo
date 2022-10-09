6a2c958962f653528c2646595b4f395e4d0238e5
Kevin <kevin.tassi@gmail.com>
2022-10-09 15:56:57 +0200

    FIX: Remove not needed useEffect

--- src/components/PokemonForm.jsx
+++ src/components/PokemonForm.jsx
@@ -1,28 +1,19 @@
-import { useEffect, useState } from 'react';
 import { Form } from 'react-bootstrap';
 
-export const PokemonForm = ({ options, onPokemonNameSelect }) => {
-  const [selectedValue, setSelectedValue] = useState('');
-
-  useEffect(() => {
-    selectedValue && onPokemonNameSelect(selectedValue);
-  }, [onPokemonNameSelect, selectedValue]);
-
-  return (
-    <Form>
-      <Form.Group className='mb-3'>
-        <Form.Label htmlFor='pokemons'>Choose a pokemon</Form.Label>
-        <Form.Select
-          id='pokemons'
-          onChange={({ target: { value } }) => {
-            setSelectedValue(value);
-          }}
-        >
-          {options.map((option) => (
-            <option value={option.value}>{option.label}</option>
-          ))}
-        </Form.Select>
-      </Form.Group>
-    </Form>
-  );
-};
+export const PokemonForm = ({ options, onPokemonNameSelect }) => (
+  <Form>
+    <Form.Group className='mb-3'>
+      <Form.Label htmlFor='pokemons'>Choose a pokemon</Form.Label>
+      <Form.Select
+        id='pokemons'
+        onChange={({ target: { value } }) => {
+          onPokemonNameSelect(value);
+        }}
+      >
+        {options.map((option) => (
+          <option value={option.value}>{option.label}</option>
+        ))}
+      </Form.Select>
+    </Form.Group>
+  </Form>
+);
